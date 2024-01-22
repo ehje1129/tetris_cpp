cde3a0a486ef5d9e18e6579926385f48f4d83a2f
SeongJiChoi <njacob654@gmail.com>
2024-01-22 17:28:28 +0900

    add basic screen

---
+++ tetris.cpp
@@ -0,0 +1,51 @@
+#include <iostream>
+#include <conio.h>
+#include <windows.h>
+
+int getKey(){
+    if (_kbhit()) {
+		return _getch();
+	}
+	return -1;
+}
+
+class StartPage {
+public:
+    StartPage() {
+        using namespace std;
+        cout << "\n\n\n\n";
+        cout << "\t\t"; cout << "@@@@@@@@@@@@  @@@@@@@@@   @@@@@@@@@@@  @@@@@@@@   @   @@@@@@@@@@@\n";
+        cout << "\t\t"; cout << "      @       @                @       @      @   @   @          \n";
+        cout << "\t\t"; cout << "      @       @                @       @      @   @   @          \n";
+        cout << "\t\t"; cout << "      @       @@@@@@@@@        @       @     @    @   @@@@@@@@@@@\n";
+        cout << "\t\t"; cout << "      @       @                @       @ @ @      @             @\n";
+        cout << "\t\t"; cout << "      @       @                @       @     @    @             @\n";
+        cout << "\t\t"; cout << "      @       @@@@@@@@@        @       @      @   @   @@@@@@@@@@@\n\n\n\n\n";
+        cout << "\t\t"; cout << "                게임을 시작하려면 아무키나 누르세요.\n\n\n\n\n\n\n";
+        while (getKey()<0) ;
+        system("cls");
+    }
+};
+
+class GameBoard {
+public:
+    GameBoard() {
+        using namespace std;
+        cout << "\n\n\n\n";
+        cout << "\t\t"; cout << "■■■■■■■■■■■■\n";
+        for (int i=0; i<30; i++){
+            cout << "\t\t"; cout << "■                    ■\n";
+        }
+        cout << "\t\t"; cout << "■■■■■■■■■■■■\n";
+        while (getKey()<0) ;
+    }
+};
+
+int main(){
+    using namespace std;
+    system("mode con cols=100 lines=40 | title 테트리스");
+    StartPage();
+    GameBoard();
+    cout << "Hellow world!";
+    getchar();
+}
